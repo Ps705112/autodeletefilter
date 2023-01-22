18fdbbb05ef4c5f54ca4b707cc26ba5bfe502626
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2023-01-22 23:39:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -235,7 +235,7 @@ async def start(client, message):
             pass
 
 
-@trojanz.on_message(filters.command('help') & filters.private)
+@trojanz.on_message(filters.command('help') & filters.private & filters.chat(ADMINS))
 async def help(client, message):
     await message.reply_text(
         text=Script.HELP_MSG,
@@ -256,7 +256,7 @@ async def help(client, message):
     )
 
 
-@trojanz.on_message(filters.command('about') & filters.private)
+@trojanz.on_message(filters.command('about') & filters.private & filters.chat(ADMINS))
 async def about(client, message):
     await message.reply_text(
         text=Script.ABOUT_MSG,
